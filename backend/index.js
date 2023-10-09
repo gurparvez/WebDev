c4e4311925c9421e5845aef01933b4fe13733a0f
Gurparvez <gurparvez04@gmail.com>
2023-10-09 20:35:35 +0530

    login authentication

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,43 @@
 require('dotenv').config()
 const express = require('express')
+const {Client} = require('pg')
+const path = require('path')
+const bodyParser = require('body-parser');
+
+const frontendDirectory = path.join(__dirname, '..', 'frontend');
+const indexPath = path.join(frontendDirectory, 'index.html');
+
 const app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const client = new Client({
+    host:"localhost",
+    user:"postgres",
+    port:"5432",
+    password:"USER1907",
+    database:"webProject"
+})
+
+client.connect();
+
 port = process.env.PORT || 5000
 
-app.get('/', (req, res)=>{
-    res.send("Hello")
+app.use(express.static(frontendDirectory));
+
+app.post('/',(req, res) => {
+    const {username, password} = req.body;
+    const query = 'SELECT * FROM users WHERE username = $1 AND password = $2';
+    const values = [username, password]
+
+    client.query(query, values, (err, result)=>{
+        if (!err && result.rows.length>0) {
+            res.send('login successful');
+        } else {
+            res.status(401).send('login failed. Invalid Credentials')
+        }
+    })
 })
 
 app.listen(port, () => {
--- frontend/script.js
+++ frontend/script.js
@@ -1,27 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
     const loginBtn = document.getElementById("loginBtn");
     const registerBtn = document.getElementById("registerBtn");
-    const authForm = document.getElementById("authForm");
 
     loginBtn.addEventListener("click", function () {
-        // Implement login logic here
-        const email = document.getElementById("email").value;
+        const username = document.getElementById("username").value;
         const password = document.getElementById("password").value;
 
-        // Example: Check if email and password are valid
-        if (email === "example@email.com" && password === "password123") {
-            alert("Login successful!");
-        } else {
-            alert("Login failed. Check your credentials.");
-        }
-    });
-
-    registerBtn.addEventListener("click", function () {
-        // Implement registration logic here
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
-
-        // Example: Register the user
-        alert(`Registered successfully! Email: ${email}`);
+        fetch('/', {
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body:JSON.stringify({ username: username, password: password }),
+        }).then((response)=>{
+            if (response.status === 200) {
+                window.location.href = '/home.html'
+            } else {
+                response.text().then((data)=>{
+                    alert(data)
+                })
+            }
+        }).catch((err)=>console.error(err))
     });
 });
